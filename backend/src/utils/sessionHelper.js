fd8e9d609ee007c795d45d7bd6143c7a61f302f1
ScorpiTUT <anas.m.abumuaileq@gmail.com>
2026-08-22 00:04:07 +0300

    Update(anas)V.16 sign is working login went very wrong

--- backend/src/utils/sessionHelper.js
+++ backend/src/utils/sessionHelper.js
@@ -1,5 +1,5 @@
 const UAParser = require('ua-parser-js');
-const AppError = require('./AppError'); 
+const AppError = require('./AppError');
 const checkRole = require('./checkvalidete');
 /**
  * @param {Object} req 
@@ -15,21 +15,15 @@ exports.extractAndValidateSessionData = (req, userRole) => {
     const user_agent = userAgentString;
     const os_name = result.os.name || 'Unknown OS';
     const browser_name = result.browser.name || 'Unknown Browser';
-    
+
     const device_id = req.headers['x-device-id'] || 'unknown-device-id';
 
     const parsedDeviceType = result.device.type;
     const isMobile = parsedDeviceType === 'mobile' || parsedDeviceType === 'tablet' || req.headers['x-client-type'] === 'mobile';
-    
+
     const device_type = isMobile ? 'mobile' : 'web';
 
-    if ( checkRole(userRole ,['user']) && device_type !== 'mobile') {
-        throw new AppError("Access Denied: Clients can only access the platform via the Xenon Mobile App.", 403);
-    }
 
-    if ( checkRole(userRole ,['vendor', 'admin']) && device_type !== 'web') {
-        throw new AppError("Access Denied: Vendors and Admins must access the platform via the Xenon Web Dashboard.", 403);
-    }
 
     return {
         ip_address,
